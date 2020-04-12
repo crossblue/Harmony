abce8d2e3b7d6627e41da50b97c396db6d047430
Kenneth Judd <judd_kenneth@hotmail.com>
2020-04-12 12:51:12 -0600

    class 3, working chat

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,22 +1,59 @@
 import { Injectable } from '@angular/core';
 import { Message } from '../models/message';
+import { Observable } from 'rxjs';
+import { AngularFirestoreCollection, AngularFirestore } from 'angularfire2/firestore';
+import { map } from  'rxjs/operators';
+import { firestore } from 'firebase';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  allMessage: Message[] = [];
+  allMessages: Observable<Message[]>;
+  messageCollection: AngularFirestoreCollection<Message>; //pipeline to firebase database
 
-  constructor() { }
+  constructor(private fb: AngularFirestore) {
+    this.messageCollection = fb.collection<Message>('posts'); // initialize connection app -> firebase
+
+   }
+
+   retrieveMessagesFromDB(){
+    this.allMessages = this.messageCollection.snapshotChanges().pipe(
+      map(actions => {
+          return actions.map(a => {
+              let data = a.payload.doc.data();
+              var d: any = data.createdOn; // <- firebase data format
+              if(d){
+                data.createdOn = new firestore.Timestamp(d.seconds, d.nanoseconds).toDate();
+              }
+              return {... data }
+          })
+      })
+    );
+  }
+
+
+   //this is the preffered method to get data back form firebase (w/o dates)
+  //  retrieveMessageFromDB(){
+  //   this.allMessages = this.messageCollection.valueChanges();
+  //  }
+
+
 
   public saveMessage(message){
-    this.allMessage.push(message);
+    var plain = Object.assign({}, message);
+    this.messageCollection.add(plain);
+
+    console.log(plain);
+    console.log(message);
 
   }
 
   public getAllMessages(){
-    return this.allMessage;
+    this.retrieveMessagesFromDB(); //subcribe to changes
+    return this.allMessages;
 
   }
 }
